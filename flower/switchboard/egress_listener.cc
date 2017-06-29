d9f61a6c8211749a64e9231ed75e36de1a9ee6e0
Ed Schouten <ed@nuxi.nl>
2017-06-29 06:38:18 +0200

    Implement the egress listener.

--- flower/switchboard/egress_listener.cc
+++ flower/switchboard/egress_listener.cc
@@ -1,13 +1,19 @@
 #include <memory>
+#include <mutex>
 
 #include <arpc++/arpc++.h>
 
+#include <flower/proto/egress.h>
 #include <flower/switchboard/egress_listener.h>
 #include <flower/switchboard/label_map.h>
 
+using arpc::ClientContext;
 using arpc::FileDescriptor;
 using arpc::Status;
 using arpc::StatusCode;
+using flower::proto::egress::Egress::Stub;
+using flower::proto::egress::ConnectRequest;
+using flower::proto::egress::ConnectResponse;
 using flower::switchboard::EgressListener;
 
 Status EgressListener::ConnectWithSocket(
@@ -19,5 +25,16 @@ Status EgressListener::ConnectWithSocket(
 
 Status EgressListener::ConnectWithoutSocket(
     const LabelMap& resolved_labels, std::shared_ptr<FileDescriptor>* fd) {
-  return Status(StatusCode::UNIMPLEMENTED, "TODO(ed): Implement!");
+  std::lock_guard<std::mutex> lock_(channel_lock_);
+  std::unique_ptr<Stub> stub = proto::egress::Egress::NewStub(channel_);
+
+  // Forward incoming connection to the server process.
+  ClientContext context;
+  ConnectRequest request;
+  *request.mutable_labels() = resolved_labels;
+  ConnectResponse response;
+  if (Status status = stub->Connect(&context, request, &response); !status.ok())
+    return status;
+  *fd = response.server();
+  return Status::OK;
 }
