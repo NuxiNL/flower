58be9b632facd105bcf6757a05999b03a8e873fb
Ed Schouten <ed@nuxi.nl>
2017-07-09 21:59:07 +0200

    Add a program_main() entry point for the switchboard.
    This allows us to easily start it on CloudABI.

---
+++ flower/switchboard/program_main.cc
@@ -0,0 +1,25 @@
+// Copyright (c) 2017 Nuxi, https://nuxi.nl/
+//
+// This file is distributed under a 2-clause BSD license.
+// See the LICENSE file for details.
+
+#include <program.h>
+
+#include <cstdlib>
+
+#include <arpc++/arpc++.h>
+
+#include <flower/switchboard/configuration.ad.h>
+#include <flower/switchboard/start.h>
+
+using arpc::ArgdataParser;
+using flower::switchboard::Configuration;
+using flower::switchboard::Start;
+
+void program_main(const argdata_t* ad) {
+  Configuration configuration;
+  ArgdataParser argdata_parser;
+  configuration.Parse(*ad, &argdata_parser);
+  Start(configuration);
+  std::exit(1);
+}
