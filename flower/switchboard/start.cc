bd363308c63d893151413dbf0ec35e323959f733
Ed Schouten <ed@nuxi.nl>
2017-07-10 15:46:47 +0200

    Add logging related code to the switchboard's entry point.

--- flower/switchboard/start.cc
+++ flower/switchboard/start.cc
@@ -3,7 +3,9 @@
 // This file is distributed under a 2-clause BSD license.
 // See the LICENSE file for details.
 
+#include <cstring>
 #include <memory>
+#include <ostream>
 #include <thread>
 
 #include <arpc++/arpc++.h>
@@ -13,6 +15,7 @@
 #include <flower/switchboard/handle.h>
 #include <flower/switchboard/start.h>
 #include <flower/switchboard/target_picker.h>
+#include <flower/util/null_ostream.h>
 #include <flower/util/socket.h>
 
 using arpc::FileDescriptor;
@@ -23,32 +26,53 @@ using flower::switchboard::Directory;
 using flower::switchboard::Handle;
 using flower::switchboard::TargetPicker;
 using flower::util::AcceptSocketConnection;
+using flower::util::null_ostream;
 
 void flower::switchboard::Start(const Configuration& configuration) {
-  // TODO(ed): Check that all required fields are present!
+  // Make logging work.
+  std::unique_ptr<std::ostream> error_log;
+  if (configuration.error_log()) {
+    // TODO(ed): Create proper logging sink.
+    error_log = std::make_unique<null_ostream>();
+  } else {
+    error_log = std::make_unique<null_ostream>();
+  }
 
-  // Start processing incoming requests.
+  // Check that all required fields are present.
+  const auto& listening_socket(configuration.listening_socket());
+  if (!listening_socket) {
+    *error_log << "Cannot start without a listening socket" << std::endl;
+    return;
+  }
+
   Directory directory;
   TargetPicker target_picker;
   for (;;) {
+    // Accept incoming connection to switchboard.
     std::unique_ptr<FileDescriptor> connection;
-    if (Status status = AcceptSocketConnection(
-            *configuration.listening_socket(), &connection, nullptr, nullptr);
+    if (Status status = AcceptSocketConnection(*listening_socket, &connection,
+                                               nullptr, nullptr);
         !status.ok()) {
-      // TODO(ed): Report error.
+      *error_log << "Failed to accept incoming connection: "
+                 << status.error_message() << std::endl;
       return;
     }
+
+    // Process incoming RPCs in a separate thread.
     // TODO(ed): Deal with thread creation errors!
     std::thread([
-      connection{std::move(connection)}, &directory, &target_picker
+      connection{std::move(connection)}, &directory, &error_log, &target_picker
     ]() mutable {
       ServerBuilder builder(std::move(connection));
       Handle handle(&directory, &target_picker);
       builder.RegisterService(&handle);
       std::shared_ptr<Server> server = builder.Build();
-      while (server->HandleRequest() == 0) {
+      int error;
+      while ((error = server->HandleRequest()) == 0) {
       }
-      // TODO(ed): Log error.
+      if (error > 0)
+        *error_log << "Failed to process incoming request: "
+                   << std::strerror(errno) << std::endl;
     }).detach();
   }
 }
