0f31d3448c502142d97881b11ba9be18fa8d0850
Ed Schouten <ed@nuxi.nl>
2017-07-23 09:27:35 +0200

    Also tighten the scope of main().

--- flower/switchboard/main.cc
+++ flower/switchboard/main.cc
@@ -25,42 +25,46 @@ using flower::util::CreateSocket;
 using flower::util::InitializeSockaddrUn;
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    std::cerr << "Usage: flower_switchboard socket_path" << std::endl;
-    return 1;
-  }
-
-  // Bind to a UNIX socket.
-  std::unique_ptr<FileDescriptor> s;
-  if (Status status = CreateSocket(AF_UNIX, &s); !status.ok()) {
-    std::cerr << status.error_message() << std::endl;
-    return 1;
-  }
-  union {
-    sockaddr sa;
-    sockaddr_un sun;
-  } path = {};
-  if (Status status = InitializeSockaddrUn(argv[1], &path.sun); !status.ok()) {
-    std::cerr << status.error_message() << std::endl;
-    return 1;
-  }
-  std::remove(argv[1]);
-  if (bind(s->get(), &path.sa, sizeof(path.sun)) != 0) {
-    std::cerr << "Failed to bind socket: " << std::strerror(errno) << std::endl;
-    return 1;
-  }
-  if (listen(s->get(), 0) != 0) {
-    std::cerr << "Failed to listen: " << std::strerror(errno) << std::endl;
-    return 1;
-  }
-
-  signal(SIGPIPE, SIG_IGN);
-
   Configuration configuration;
-  configuration.set_listening_socket(std::move(s));
-  configuration.set_logger_output(
-      std::make_unique<FileDescriptor>(STDERR_FILENO));
-  // TODO(ed): Make this configurable.
-  configuration.set_worker_pool_size(10);
+  {
+    if (argc != 2) {
+      std::cerr << "Usage: flower_switchboard socket_path" << std::endl;
+      return 1;
+    }
+
+    // Bind to a UNIX socket.
+    std::unique_ptr<FileDescriptor> s;
+    if (Status status = CreateSocket(AF_UNIX, &s); !status.ok()) {
+      std::cerr << status.error_message() << std::endl;
+      return 1;
+    }
+    union {
+      sockaddr sa;
+      sockaddr_un sun;
+    } path = {};
+    if (Status status = InitializeSockaddrUn(argv[1], &path.sun);
+        !status.ok()) {
+      std::cerr << status.error_message() << std::endl;
+      return 1;
+    }
+    std::remove(argv[1]);
+    if (bind(s->get(), &path.sa, sizeof(path.sun)) != 0) {
+      std::cerr << "Failed to bind socket: " << std::strerror(errno)
+                << std::endl;
+      return 1;
+    }
+    if (listen(s->get(), 0) != 0) {
+      std::cerr << "Failed to listen: " << std::strerror(errno) << std::endl;
+      return 1;
+    }
+
+    signal(SIGPIPE, SIG_IGN);
+
+    configuration.set_listening_socket(std::move(s));
+    configuration.set_logger_output(
+        std::make_unique<FileDescriptor>(STDERR_FILENO));
+    // TODO(ed): Make this configurable.
+    configuration.set_worker_pool_size(10);
+  }
   Start(configuration);
 }
